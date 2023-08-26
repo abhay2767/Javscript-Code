9e678fcf8230e0a4060a1df6ba63080ccbfe85f8
Abhay <abhaydubey2767@gmail.com>
2023-08-26 16:09:04 +0530

    done

--- otp.js
+++ otp.js
@@ -1,6 +1,6 @@
 const r = Math.random()
 console.log(r)
 //r; 
-const otp = Math.round(r*9000+1000)
+const otp = Math.round(r*900000+100000) //6 dight under 1000000
 //otp;
 console.log("Otp is: "+otp)
