8734fd14a1813cc023f3644ff31e1ed84456d44a
Abhay <abhaydubey2767@gmail.com>
2023-09-30 23:34:55 +0530

    done

---
+++ 46_Express.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/47_index.html')) //serve html file
+}) 
+app.get('/', (req, res) => {
+    console.log(req.query.name)
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port  http://localhost:${port}/`)
+})
